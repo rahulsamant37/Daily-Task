14ff89e1737d0972537abca73d4c8478dd97d750
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-03-05 09:44:53 +0530

    Add Python question: D:\Rahul-Github\Daily-Task\Python\python_question_45.py

---
+++ Python/python_question_45.py
@@ -0,0 +1,6 @@
+# Python Question: Calculate the sum of natural numbers from k to n
+'''
+Find the sum of natural numbers from k to n. For example, if k = 3 and n = 6, the sum should be 15.
+
+Input: k = 3, n = 6
+Output: 15 (3 + 4 + 5)
