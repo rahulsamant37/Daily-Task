9909459dcc388ce9d19eaadf07316daf2addb5cb
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-03-07 13:37:39 +0530

    Add Python question: python_question_57.py

---
+++ Python/python_question_57.py
@@ -0,0 +1,7 @@
+# Python Question: Fibonacci Sequence without Recursion
+'''
+Given two positive integers, n and k, where n >= k, calculate the k-th number in the Fibonacci sequence after the n-th number.
+
+Example:
+Input: n = 6, k = 3
+Output: 5 (3rd number in the Fibonacci sequence starting from the 6th number)
