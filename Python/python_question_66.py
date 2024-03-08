2676d08c9cc4ee8c37e594dbb4edd1b8c5f0828f
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-03-08 14:38:32 +0530

    Add Python question: python_question_66.py

---
+++ Python/python_question_66.py
@@ -0,0 +1,11 @@
+# Python Question: Calculate the largest number that can be formed using the digits of a given number
+'''
+Consider a list of digits, each representing a digit from 0 to 9. Find the largest number that can be formed using these digits without repetition.
+
+Example:
+
+Input: [2, 4, 8]
+Output: 248 (The largest number using the given digits without repetition)
+
+Input: [1, 2, 3, 5, 6]
+Output: 98356 (The largest number using the given digits without repetition)
