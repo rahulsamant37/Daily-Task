f20a8e83c491cce10f6b607ba489d995e3443018
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-03-08 12:24:45 +0530

    Add Python question: python_question_61.py

---
+++ Python/python_question_61.py
@@ -0,0 +1,33 @@
+# Python Question: Print all possible words that can be formed using given letters
+
+# Problem Statement
+'''
+You are given a string containing only digits and some special characters. Find all possible words that can be formed using the given letters. The length of the words can be 2 or more.
+
+For example,
+
+Input:  ABCDEFGHIJKLMNOPQRSTUVWXYZ
+Output: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+Input:  ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':\",.<>/?`~
+Output: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '/', '?', '`', '~']
+'''
+
+def find_words(letters):
+    # Create a set of all unique letters
+    letters_set = set(letters)
+
+    # Create an empty list to store the result
+    result = []
+
+    # Iterate through all possible combinations of letters
+    for i in range(1 << len(letters)):
+        combination = ''.join(str(letters_set.get(x, '_') for x in i)
+        result.append(combination)
+
+    return result
+
+# Example
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(find_words(letters))
