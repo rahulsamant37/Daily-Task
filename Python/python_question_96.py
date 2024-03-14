b2d1105f81986c51a2d6f379e634b20603fab5cb
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-03-14 09:11:38 +0530

    Add Python question: python_question_96.py

---
+++ Python/python_question_96.py
@@ -0,0 +1,11 @@
+# Python Question: Fibonacci Series till a given number using recursion
+'''
+The Fibonacci series is a sequence of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1. Write a function that takes an integer as input and returns the Fibonacci series till that number using recursion.
+
+Example:
+
+Input: n = 5
+Output: [0, 1, 1, 2, 3]
+
+Input: n = 8
+Output: [0, 1, 1, 2, 3, 5, 8]
