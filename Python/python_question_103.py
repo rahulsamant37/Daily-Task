58281bec6fe0d7687113756a767838e40738db8d
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-03-15 11:46:42 +0530

    Add Python question: python_question_103.py

---
+++ Python/python_question_103.py
@@ -0,0 +1,13 @@
+# Python Question: Finding the longest substring common to two given strings
+'''
+Given two strings, find the length of the longest substring that is common to both strings. If no such substring exists, return 0.
+
+Example:
+Input: A = "abcdef", B = "acdef"
+Output: 3 (The longest common substring is 'ac')
+
+Input: A = "abcef", B = "edc"
+Output: 2 (The longest common substring is 'ec')
+
+Input: A = "abcd", B = "ab"
+Output: 2 (The longest common substring is 'ab')
