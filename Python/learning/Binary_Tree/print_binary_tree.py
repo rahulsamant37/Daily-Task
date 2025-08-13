dc4784bbfae4c8c9cff050510371b05351eedb65
Rahul Samant <rahulsamantcoc2@gmail.com>
2025-08-14 02:50:00 +0530

    feat: Creating function to demontrate the print of binary tree with recursion

---
+++ Python/learning/Binary_Tree/print_binary_tree.py
@@ -0,0 +1,20 @@
+from Generic_Binary_Tree_Input import predefine_binary_tree_inputs
+
+def print_binary_tree(root):
+    if root is None:
+        return
+    print(root.data, end=" :----------> ")
+    if root.left is not None:
+        print(f"L-> {root.left.data}", end=", ")
+    else:
+        print("L-> None", end=", ")
+    if root.right is not None:
+        print(f"R-> {root.right.data}", end=", \n")
+    else:
+        print("R-> None", end=", \n")
+    print_binary_tree(root.left)
+    print_binary_tree(root.right)
+
+root1, root2, root3 = predefine_binary_tree_inputs()
+
+print_binary_tree(root1)
