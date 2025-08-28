96ada57592fe8fed3fa4cdb3269b206c34f1ef0e
Rahul Samant <rahulsamantcoc2@gmail.com>
2025-08-29 02:04:04 +0530

    refactor: modularize BST printing implementation
    - Remove inline BSTNode class definition
    - Import TreeNode and create_predefined_bsts from helper module
    - Convert print_bst from method to standalone function
    - Add demo usage with predefined BST structures
    - Improves code reusability and separation of concerns

--- Python/learning/BST/001_BST_Node_Printing.py
+++ Python/learning/BST/001_BST_Node_Printing.py
@@ -1,14 +1,13 @@
-class BSTNode:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+from helper import create_predefined_bsts
 
-    def print_bst(self, root):
-        if (root is None):
-            return
-        
-        self.print_bst(root.left)
-        print(root.data, end=" ") ## Inorder Traversal of my BST
-        self.print_bst(root.right)
+def print_bst(root):
+    if (root is None):
+        return
+    
+    print_bst(root.left)
+    print(root.data, end=" ") ## Inorder Traversal of my BST
+    print_bst(root.right)
 
+root1, root2, root3 = create_predefined_bsts()
+
+print_bst(root1)
