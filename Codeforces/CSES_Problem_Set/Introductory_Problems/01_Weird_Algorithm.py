e661392eb18f50788712a73219e1fa737ad734ee
Rahul Samant <rahulsamantcoc2@gmail.com>
2025-09-08 09:27:39 +0530

    feat: This commit adds a function to generate the Collatz sequence (also known as the 'Weird Algorithm') for a given positive integer . The sequence is printed to standard output as space-separated values.

---
+++ Codeforces/CSES_Problem_Set/Introductory_Problems/01_Weird_Algorithm.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+def solve():
+    n = int(input())
+    print(n, end=" ")
+    while n>1:
+        if n % 2 == 0:
+            n = n // 2
+            print(n, end=" ")
+        else:
+            n = 3*n+1
+            print(n, end=" ")
+if __name__ == "__main__":
+    solve()
