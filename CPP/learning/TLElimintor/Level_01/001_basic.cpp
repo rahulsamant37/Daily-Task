abbfb3d8c8a64352aceea0a1d82a9523463393de
Rahul Samant <rahulsamantcoc2@gmail.com>
2025-10-06 03:05:05 +0530

    feat: Add Week 1 basic C++ practice file for TLE Eliminator
    Includes:
    - Examples of input/output
    - Pre/post increment behavior
    - Basic for loops
    - Character and arithmetic operations
    - String traversal
    - Comments for concept clarification
    
    Note: Contains commented-out multiple  functions for modular learning.

---
+++ CPP/learning/TLElimintor/Level_01/001_basic.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+// int main() {
+//     string rahul = "Rahul";
+//     cout << sqrt(4);
+//     cout << rahul;
+// }
+
+// int main() {
+//     // + and ++ basically have nothing in common
+//     // - and -- basically have nothing in common 
+
+//     // and this ++a and a++ is different as it is pre and post increment
+//     int a = 10;
+//     int b = 10;
+//     cout << "--------### Preoder Increment ###--------" << endl;
+//     cout << ++a << endl;
+//     cout << b++ << endl;
+    
+//     cout << "--------### Postoder Increment ###---------" << endl;
+//     cout << a << endl;
+//     cout << b << endl;
+// }
+
+// int main() {
+//     for (int i = 0; i<5;i++) {
+//         cout << i << endl;
+//     }
+// }
+
+// //Take Input
+// int main() {
+//     int a, b, c;
+//     cin >> a >> b >> c;
+//     cout << a << b << c;
+//     return 0;
+// }
+
+// // FOr Loop
+// int main() {
+//     string a;
+//     cin >> a;
+//     for (int i = 0; i < a.length(); i++){
+//         cout << a[i] << endl;
+//     }
+// }
+
+
+// // Assignment 001
+// int main() {
+//     char !;
+
+// }
+
+// // Assignmenet 002
+// int main() {
+//     int a = 10;
+//     int b = 20;
+//     cout << a+b << endl;
+//     cout << a-b << endl;
+//     cout << (float) a/b << endl;
+// }
