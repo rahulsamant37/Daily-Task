d1e990adca4af8e1786f4706ddb1cac77e790cdd
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-09 18:11:35 +0530

    feat: Add basic C++ program for input/output demonstration

---
+++ youtube/freecodecamp-org/learn-cpp/01-basic/02-variables.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+int main() {
+    string str;
+    int i;
+    long long ll;
+    float f;
+    double d;
+    char c;
+    bool b;
+
+    cin >> str >> i >> ll >> f >> d >> c >> b;
+
+    cout << "Hello, " << str << endl;
+    cout << i << endl;
+    cout << ll << endl;
+    cout << f << endl;
+    cout << d << endl;
+    cout << c << endl;
+    cout << b << endl;
+
+    return 0;
+}
