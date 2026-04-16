936fc565f57489eae4eb5212c558810fbea2fdd0
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-16 11:48:36 +0530

    feat(learn-java): add datatype.java example

---
+++ learn-language/learn-java/01-basic/datatype.java
@@ -0,0 +1,13 @@
+public class datatype {
+	public static void main(String[] args){
+		byte num = 127;
+		num+=1;
+		System.out.println(num+1);
+		num+=1;
+		System.out.println(num+1);
+		num+=1;
+		System.out.println(num+1);
+		num+=1;
+		System.out.println(num+1);
+	}
+}
