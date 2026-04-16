3a4aa25b34a5a3c6e9576fe0439b3f7bedad8e61
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-16 11:48:29 +0530

    feat(learn-cpp): add test.cpp

---
+++ learn-language/learn-cpp/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+int func(void) {
+    return 0;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << func();
+    return 0;
+}
