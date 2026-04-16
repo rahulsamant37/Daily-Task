888e897422f5842d3103648d698e00a601c7b929
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-16 11:49:10 +0530

    feat(udemy-abdul-bari): add 01-array.cpp

---
+++ udemy-course/abdul-bari/master-dsa-using-c-and-cpp/01-essential/01-array.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+int main() {
+    int a[5];
+    int b[5] = {2, 3, 4, 5, 6};
+    for (int i: b) {
+        cout << i << endl;
+    }
+    cout << sizeof(a);
+    return 0;
+}
