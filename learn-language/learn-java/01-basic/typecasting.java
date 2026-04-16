6adc361acc0d184f61313fa06c836d7276f5ca3c
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-16 11:48:59 +0530

    feat(learn-java): add typecasting.java example

---
+++ learn-language/learn-java/01-basic/typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting {
+	public static void main(String[] args) {
+		float f = 259.2f;
+		int x = (int)f;
+		System.out.println(x);
+		byte y = (byte)x;
+		System.out.println(y);
+	}
+}
