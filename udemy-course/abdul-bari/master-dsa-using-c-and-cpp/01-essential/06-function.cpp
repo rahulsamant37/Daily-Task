914715d3f8dbcd720efbe142c59141b9be27bacf
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-19 05:28:17 +0530

    feat: add function example (06-function.cpp)

---
+++ udemy-course/abdul-bari/master-dsa-using-c-and-cpp/01-essential/06-function.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+int add(int a, int b) {
+    return a+b;
+}
+
+int main() {
+    int a = 10, b = 15;
+    cout << add(a, b);
+    return 0;
+}
