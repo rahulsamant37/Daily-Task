1c713d7437b3d151f51d3ac17b7d08b085660126
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-07-11 08:13:21 +0530

    feat: add solution for atcoder 463 a-16-9

---
+++ contest/atcoder/contest-463/a-16-9.cpp
@@ -0,0 +1,48 @@
+/*
+Author: Rahul Samant
+Created: 2026-06-20 17:49:28
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int x, y;
+    cin >> x >> y;
+    if (9*x == 16*y) {
+        cout << "Yes";
+    } else { 
+        cout << "No";
+    }
+    return 0;
+}
