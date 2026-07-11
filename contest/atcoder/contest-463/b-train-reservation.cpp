5795533df30f637a964877b34a0d96e939589de5
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-07-11 08:13:21 +0530

    feat: add solution for atcoder 463 b-train-reservation

---
+++ contest/atcoder/contest-463/b-train-reservation.cpp
@@ -0,0 +1,55 @@
+/*
+Author: Rahul Samant
+Created: 2026-06-20 17:56:16
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    int n;
+    char x;
+    cin >> n >> x;
+    vector<char> arr(5*n);
+    rep(i, 0, 5*n) cin >> arr[i];
+    int i = x - 65;
+    while (i<5*n) {
+        if(arr[i] == 'o') {
+            cout << "Yes";
+            return 0;
+        }
+        i+=5;
+    }
+    cout << "No";
+    return 0;
+}
