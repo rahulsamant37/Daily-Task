a034c62e39c82615f17b306dd7666d1d1c9eb00f
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-07-11 09:53:50 +0530

    chore: Improve demonstration of reference behavior after assignments

--- udemy-course/abdul-bari/master-dsa-using-c-and-cpp/01-essential/04-reference.cpp
+++ udemy-course/abdul-bari/master-dsa-using-c-and-cpp/01-essential/04-reference.cpp
@@ -6,12 +6,18 @@ using namespace std;
 int main() {
     int a = 10;
     int &r =a; // reference needed to initialized otherwise it show error
-    cout << a << endl << r << endl;
+    cout << "a = " << a << endl << "r = " << r << endl;
+    cout << endl;
+    cout << "========== after a = 12 ============" << endl;
+    cout << endl;
     a = 12;
-    cout << a << endl << r << endl;
+    cout << "a = " << a << endl << "r = " << r << endl;
+    cout << endl;
+    cout << "========== after r = b = 11 ============" << endl;
+    cout << endl;
     int b =11;
     r = b;  // this will not change the reference instead change the value at that address
-    cout << a << endl << r << endl;
-    cout << b << endl;
+    cout << "a = " << a << endl << "r = " << r << endl;
+    cout << "b = " << b << endl;
     return 0;
 }
