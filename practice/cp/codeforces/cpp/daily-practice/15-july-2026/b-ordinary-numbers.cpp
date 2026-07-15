53bbb6ea4fbca87507246e65c2e45304b26dc393
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-07-15 23:26:44 +0530

    refactor(practice): update logic for b-ordinary-numbers

--- practice/cp/codeforces/cpp/daily-practice/15-july-2026/b-ordinary-numbers.cpp
+++ practice/cp/codeforces/cpp/daily-practice/15-july-2026/b-ordinary-numbers.cpp
@@ -38,28 +38,19 @@ void setup_io() {
 void solve() {
     ll n;
     cin >> n;
-    ll m = n;
-    if(n<10) {
-        cout << n << endl;
-        return;
-    }
-    ll cnt = 0;
+    string str = to_string(n);
+    int len = str.size();
+    int first = str[0] - '0';
     ll comp = 0;
-    
-    while((n/10)>0) {
-        cnt++;
-        n/=10;
+    for (int i = 0; i<len; i++) {
+        comp = comp * 10 + first;
     }
-    for (int i = 0; i<cnt; i++) {
-        comp = comp + (10+n);
-    }
-    ll ans;
-    if (comp<m) {
-        ans = 1 + 9*cnt;
+    int ans = 9 * (len - 1);
+    if(comp<=n) {
+        ans+=first;
     } else {
-        ans = 9*cnt;
+        ans+=first-1;
     }
-
     cout << ans << endl;
 }
 
