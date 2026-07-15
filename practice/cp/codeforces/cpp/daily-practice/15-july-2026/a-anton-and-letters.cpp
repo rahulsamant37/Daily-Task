88ce553cd167eeddfcd7d3faaf36118828648851
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-07-15 23:17:18 +0530

    feat(practice): add solution for Anton and Letters

---
+++ practice/cp/codeforces/cpp/daily-practice/15-july-2026/a-anton-and-letters.cpp
@@ -0,0 +1,50 @@
+/*
+Author: Rahul Samant
+Created: 2026-07-15 21:45:24
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define endl '\n'
+
+#ifdef LOCAL
+#define debug(x) cerr << "[DEBUG] " << #x << " = " << (x) << endl
+#else
+#define debug(x)
+#endif
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void setup_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    setup_io();
+    string str;
+    getline(cin, str);
+    set<char> s;
+    for(char ch: str) {
+        if(isalpha(ch)) {
+            s.insert(ch);
+        }
+    }
+    cout << s.size();
+    return 0;
+}
